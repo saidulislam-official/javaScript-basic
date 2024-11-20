68e6c3c84f954d47caaaddae81ec88dd8d7cc18d
saidulislam-official <saidulislam.own@gmail.com>
2024-11-20 10:42:56 +0600

    added array

---
+++ array/array.js
@@ -0,0 +1,15 @@
+/**
+const num1 = 22;
+const num2 = 22;
+const num3 = 22;
+const num4 = 22;
+const num5 = 22;
+ */
+
+const numbers = [6,16, 26, 36, 66, 506,666];
+console.log(numbers);
+const friends = ['abul', 'babul', 'cabul', 'dabul', 'fabul'];
+console.log(friends);
+const passed = [true, false, true, false, true];
+console.log(passed);
+const mixedArray = ['abul', 22, true, 33, 'babul', false, 44];
